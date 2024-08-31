497d0bd6cfc5053add377c7ed0ea648714356371
jabessbuscay <144251410+jabessbuscay@users.noreply.github.com>
2024-08-31 20:03:27 +0800

    Update

---
+++ grp5_neiljabess_exp2.py
@@ -0,0 +1,48 @@
+num1 = 19  
+num2 = 5
+
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = num1 % num2
+result_exp = num1 ** num2
+
+print("Addition: ", result_add)
+print("Subtraction: ", result_sub)
+print("Multiplication: ", result_mul)
+print("Division: ", result_div)
+print("Modulus: ", result_mod)
+print("Exponent: ", result_exp)
+
+num1 = 20.5
+num2 = 7.5
+
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = num1 % num2
+result_exp = num1 ** num2
+
+print("\nAddition (float): ", result_add)
+print("Subtraction (float): ", result_sub)
+print("Multiplication (float): ", result_mul)
+print("Division (float): ", result_div)
+print("Modulus (float): ", result_mod)
+print("Exponent (float): ", result_exp)
+
+num1 = 3 + 4
+num2 = 2 + 5
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = abs(num1 - num2)  
+result_exp = num1 ** 0.5  
+print("\nAddition (complex): ", result_add)
+print("Subtraction (complex): ", result_sub)
+print("Multiplication (complex): ", result_mul)
+print("Division (complex): ", result_div)
+print("Modulus (complex): ", result_mod)
+print("Exponent (complex): ", result_exp)
