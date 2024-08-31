49d227a5f028ca0c0c7e2787870274371bbd6298
jabessbuscay <144251410+jabessbuscay@users.noreply.github.com>
2024-08-31 21:36:52 +0800

    Update grp5_neiljabess_exp2.py

--- grp5_neiljabess_exp2.py
+++ grp5_neiljabess_exp2.py
@@ -1,3 +1,5 @@
+#Create a Python program to perform arithmetic Operations  (Addition, Subtraction, Division, Multiplication, Modulus, Exponent) using two variable numbers for int, float and complex
+
 num1 = 19  
 num2 = 5
 
@@ -46,3 +48,23 @@ print("Multiplication (complex): ", result_mul)
 print("Division (complex): ", result_div)
 print("Modulus (complex): ", result_mod)
 print("Exponent (complex): ", result_exp)
+
+
+#Write a program that creates two variables, num1 and num2. Both num1 and num2 should be assigned the integer literal 25000000, one written with underscores and one without. Print num1 and num2 on two separate lines.
+
+num1 = 25_000_000 
+num2 = 25000000 
+
+print(num1)
+print(num2)
+
+
+#Creating int,float and complex variable then check the type using the Python Code
+
+int_var = 19
+float_var = 19.0
+complex_var = 19 + 0
+
+print(f"Type of int_var: {type(int_var)}")
+print(f"Type of float_var: {type(float_var)}")
+print(f"Type of complex_var: {type(complex_var)}")
