565b8ec120e27fe2742deb2aaef2dacdd33270f3
jabessbuscay <144251410+jabessbuscay@users.noreply.github.com>
2024-08-31 21:57:00 +0800

    Update

---
+++ grp5_neiljabess_exp1.py
@@ -0,0 +1,24 @@
+ 
+print("Hello, I'm Neil Jabess Buscay! BSCS 3G.")
+
+number = 21
+name = "Jabess"
+
+# b. Then print both values
+print("Number:", number)
+print("Name:", name)
+
+
+Recto = 20
+Marry = 88
+Tristan = 175
+Jiggs = "Singing"
+
+
+apple, banana, cherry = "Red", "Yellow", "Dark Red"
+
+
+x = y = z = 100
+
+print(apple, banana, cherry)
+print(x, y, z)
